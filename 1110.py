e132f574e0c10be12bc7613c1ea20b29dc35e56c
2riing <2riing2@gmail.com>
2022-02-06 23:31:42 +0900

    1110

--- 1110.py
+++ 1110.py
@@ -9,8 +9,23 @@
 # 1 +  
 
 
-N = int(input())
 
-def count_ci(N):
-    pass
 
+N = input()
+cnt = 0
+num = N
+while True:
+    if len(num)==1:
+        num = "0" + num 
+        # num type str
+    else:
+        plus = int(num[0]) + int(num[-1]) 
+        # 중간과정을 의미함 앞자리수 + 뒷자리수 
+        # plus의 타입 : int
+        num = num[-1] + str(plus)[-1]
+        # 새로운 수를 저장함 str : num의 뒷자리 + plus뒷자리 
+        cnt += 1
+        if int(num) == int(N):
+            print(cnt)
+            break
+    
